a82f814185827f4dceba717231436887326070d7
Majster96PL <piotruniok96@gmail.com>
2023-01-03 13:20:39 +0100

    Create model user

---
+++ src/main/java/com/example/tracker/user/User.java
@@ -0,0 +1,29 @@
+package com.example.tracker.user;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String UUID;
+    @Column(name = "firstname")
+    @NotBlank(message = "Required firstname!")
+    private String firstname;
+    @Column(name = "lastname")
+    @NotBlank(message = "Required lastname!")
+    private String lastname;
+    @Email
+    @NotBlank(message = "Required email!")
+    private String email;
+    private String login;
+    private String password;
+}
