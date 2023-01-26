c96d3c84804b8f58ef92d211132b455a0063f4ee
Majster96PL <piotruniok96@gmail.com>
2023-01-26 13:31:54 +0100

    Create model role for user

--- src/main/java/com/example/tracker/user/User.java
+++ src/main/java/com/example/tracker/user/User.java
@@ -3,45 +3,41 @@ package com.example.tracker.user;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
+import java.util.HashSet;
+import java.util.Set;
+
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table( name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String UUID;
-    @Column(name = "firstname")
-    @NotBlank(message = "Required firstname!")
-    private String firstname;
-    @Column(name = "lastname")
-    @NotBlank(message = "Required lastname!")
-    private String lastname;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ID;
+    @NotBlank(message = "Required username!")
+    @Size(max = 20)
+    private String username;
     @Email
     @NotBlank(message = "Required email!")
+    @Size(max = 50)
     private String email;
-    private String login;
+    @NotBlank
+    @Size(max = 120)
     private String password;
-    @Enumerated(EnumType.STRING)
-    private UserRole role;
-    private boolean lockedUser;
-    private boolean enabledUser;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
-    public User(String firstname,
-                String lastname,
-                String email,
-                String login,
-                String password,
-                UserRole role) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.email = email;
-        this.login = login;
-        this.password = password;
-        this.role = role;
-    }
 }
