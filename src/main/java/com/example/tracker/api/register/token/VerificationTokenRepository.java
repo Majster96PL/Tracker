7f8a155b49a3266de7c396552713fbc5d8ed420d
Majster96PL <piotruniok96@gmail.com>
2023-02-21 23:17:53 +0100

    Create token repository

---
+++ src/main/java/com/example/tracker/api/register/token/VerificationTokenRepository.java
@@ -0,0 +1,20 @@
+package com.example.tracker.api.register.token;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<Long, VerificationToken> {
+    Optional<VerificationToken> findByToken(String token);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Token t SET t.confirmed = ?2 WHERE t.token = ?1")
+    int updateToken(String token, LocalDateTime confirmed);
+}
