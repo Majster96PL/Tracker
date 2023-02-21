a1b7319c46eb8c6380e6cf80f85e8a72d8201894
Majster96PL <piotruniok96@gmail.com>
2023-02-21 22:44:17 +0100

    Create service with implementation UserDetailsService

--- src/main/java/com/example/tracker/api/register/user/UserRepository.java
+++ src/main/java/com/example/tracker/api/register/user/UserRepository.java
@@ -1,13 +1,19 @@
 package com.example.tracker.api.register.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long > {
     Optional<User> findByUsername(String username);
-    Boolean existsByUsername(String username);
-    Boolean existsByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User u SET u.enabled = TRUE WHERE u.email = ?1")
+    int enabledUser(String email);
 }
--- src/main/java/com/example/tracker/api/register/user/UserService.java
+++ src/main/java/com/example/tracker/api/register/user/UserService.java
@@ -1,4 +1,38 @@
 package com.example.tracker.api.register.user;
 
-public class UserService {
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private static final String MESSAGE = "USER NOT FOUND!";
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> findUser = userRepository.findByUsername(username);
+        User user = findUser.orElseThrow(
+                () -> new UsernameNotFoundException(String.format(MESSAGE, username))
+
+        );
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.isEnabled(),
+                true,
+                true,
+                true,
+                user.getAuthorities()
+        );
+    }
+
+    public int enabledUser(String email){
+        return userRepository.enabledUser(email);
+    }
+
 }
